58f1427aabaab5b8a65bacc02e79d1f55b72e966
demo <demo@gmail.com>
2021-10-01 23:52:06 +0800

    first commit

---
+++ vsctry.py
@@ -0,0 +1,17 @@
+score = int(input("請輸入數學分數 (0 ~ 100)："))
+if score >= 90:
+    print("優等")
+elif score < 90 and score >= 80:
+    print("甲等")
+elif score < 80 and score >= 70:
+    print("乙等")
+elif score < 70 and score >= 60:
+    print("丙等")
+else:
+    print("不及格")
+
+i = 0
+while i <= 5:
+    print(i)
+    i += 1      
+print("優等")
